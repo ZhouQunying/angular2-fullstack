4ae8ad0d0adc48905f77849d6111127018daf93f
ZhouQunying <1801886869@qq.com>
2016-01-10 18:50:01 +0800

    add .babelrc file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,24 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
+// const gulp = require('gulp');
+// const babel = require('gulp-babel');
+// const sourcemaps = require('gulp-sourcemaps');
+// const concat = require('gulp-concat');
+	
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
+
+const dirs = {
+	src: 'src',
+	dest: 'dist'
+}
 
 gulp.task('default', () => {
     return gulp.src('server/**/*.js')
         .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+        // .pipe(babel({
+        //     presets: ['es2015']
+        // }))
         .pipe(concat('main.js'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
