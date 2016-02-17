82b967c43df10b498266e497065b395a1298fe64
zhouqunying <frank.zhou@huanxunedu.com>
2016-02-17 18:11:09 +0800

    js

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import crypto from 'crypto';
-var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+import Promise from 'bluebird';
+var mongoose = Promise.promisifyAll(require('mongoose'));
 import {Schema} from 'mongoose';
+
