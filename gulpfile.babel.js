eb145d7bdee8ab7ee441a01bdc1138a7612eb790
ZhouQunying <1801886869@qq.com>
2016-04-20 18:47:03 +0800

    something base

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,25 @@
 'use strict';
 
+import gulp from 'gulp';
 import _ from 'lodash';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const clientPath = 'client';
+const serverPath = 'server';
+const paths = {
+  client: {
+    mainView: `${clientPath}/index.html`
+  },
+  server: {},
+  dist: 'dist'
+};
+const plugins = gulpLoadPlugins();
+
+gulp.task('inject', cb => {
+  plugins.runSequence(['inject:js', 'inject:css', 'inject:scss'], cb);
+});
+
+gulp.task('inject:js', () => {
+  return gulp.src(paths.client.mainView)
+    .pipe(plugins.inject());
+});
