295e21f73e63a42e5c531264c70b49a1d084412a
ZhouQunying <1801886869@qq.com>
2016-04-28 09:34:14 +0800

    gulpfile

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,1 +1,166 @@
-console.log('test')
+/**
+ * author : Ryan Liu ryanliu@shunshunliuxue.com
+ * date   : 10 Dec 2015
+ * desc   : final case list controller
+ **/
+
+(function() {
+  angular.module("BI").controller("FinalCaseListCtrl", function(Auth, $state, Restangular, FinalCaseDataFields, $window, $timeout, Config, Utils, $sce) {
+    var restrictsHasValue, vm;
+    vm = this;
+    vm.Auth = Auth;
+    vm.now = new Date();
+    vm.pageConf = {
+      maxSize: 5,
+      currentPage: $state.params.page || 1,
+      itemsPerPage: 10,
+      sort: $state.params.sort,
+      filter: $state.params.filter,
+      keywords: $state.params.keywords
+    };
+    if (vm.pageConf.filter) {
+      try {
+        vm.filters = angular.fromJson(vm.pageConf.filter);
+      } catch (_error) {
+      }
+    } else {
+      vm.filters = {};
+    }
+    vm.restricts = Auth.getCurrentUser().permission.filters;
+    restrictsHasValue = _.values(vm.restricts).some(function (value) {
+      return value || value === 0;
+    });
+    if (!restrictsHasValue) {
+      vm.restricts = null;
+    }
+
+
+    vm.minDate = new Date('1980-01-01');
+    vm.maxDate = new Date('2020-12-31');
+    vm.today = Math.floor((new Date).setHours(0) / 3600000) * 3600000;
+    vm.todayLater = Math.ceil((new Date).setHours(23) / 3600000) * 3600000 - 1;
+    vm.disableFilter = function (field) {
+      var ref, ref1;
+      return ((ref = vm.restricts) != null ? ref[field.name] : void 0) || ((ref1 = vm.restricts) != null ? ref1[field.name] : void 0) === 0;
+    };
+    if (vm.pageConf.sort) {
+      try {
+        vm.sorts = angular.fromJson(vm.pageConf.sort);
+      } catch (_error) {
+      }
+    } else {
+      vm.sorts = ['-create_at'];
+    }
+    vm.query = vm.pageConf.keywords;
+    vm.resetMultiFilter = function (field) {
+      if (vm.filters[field.name] && vm.filters[field.name].length === 0) {
+        return delete vm.filters[field.name];
+      }
+    };
+    vm.reload = function (resetIndex) {
+      if (resetIndex) {
+        vm.pageConf.currentPage = 1;
+      }
+      _.forEach(vm.filters, function (value, key) {
+        if (value === null) {
+          return delete vm.filters[key];
+        }
+      });
+      vm.pageConf.filter = angular.toJson(vm.filters);
+      vm.pageConf.sort = angular.toJson(vm.sorts);
+      return $state.go('finalCase.list', {
+        page: vm.pageConf.currentPage,
+        sort: vm.pageConf.sort,
+        filter: vm.pageConf.filter,
+        keywords: vm.pageConf.keywords
+      });
+    };
+    vm.resetQuery = function () {
+      vm.pageConf.currentPage = 1;
+      delete vm.pageConf.sort;
+      delete vm.pageConf.filter;
+      delete vm.pageConf.keywords;
+      return vm.reload();
+    };
+    vm.fields = FinalCaseDataFields;
+
+    vm.dateOptions = {
+      formatYear: 'yyyy',
+      startingDay: 1,
+      'show-weeks': false
+    };
+    vm.prevent = function ($event) {
+      $event.preventDefault();
+      return $event.stopPropagation();
+    };
+    vm.search = function (status) {
+      var base, base1, base2, base3, filterObj;
+      filterObj = {};
+      try {
+        if (vm.pageConf.filter) {
+          filterObj = angular.fromJson(vm.pageConf.filter);
+        }
+      } catch (_error) {
+      }
+      vm.fields.forEach(function (field) {
+        return field.$filter = filterObj[field.name];
+      });
+      vm.pageConf.sort = angular.toJson(vm.sorts);
+      return Restangular.all('finalcase/').getList({
+        page: vm.pageConf.currentPage - 1,
+        keywords: vm.pageConf.keywords,
+        order_by: angular.toJson(vm.sorts),
+        filters: angular.toJson(filterObj),
+        length: vm.pageConf.itemsPerPage
+      }).then(function (result) {
+        var isFin = Auth.getCurrentUser().permission.is_financial_officer;
+        _.map(result, function(item) {
+          if (item['final_status']=='待审核') {
+            if (isFin && item['final_type'] == '异常结案'){
+              item.canApprove = true;
+            } else if (!isFin && item['final_type'] == '正常结案') {
+              item.canApprove = true;
+            }
+          }
+
+          //item.remarkHtml = $sce.trustAsHtml(item.remark);
+
+          return item;
+        });
+        return vm.entities = result;
+      });
+    };
+    vm.pending = function() {
+      var filterObj = {};
+      var filter = 'final_status';
+      var status = '待审核';
+      filterObj[filter] = status;
+      vm.filters[filter] = status;
+      vm.pageConf.filter = angular.toJson(filterObj);
+      vm.pageConf.keywords = vm.query;
+      vm.reload(true)
+    };
+    vm.download = function() {
+      var exportUrl;
+      vm.holding = true;
+      $timeout(function() {
+        return vm.holding = false;
+      }, 3000);
+      exportUrl = Config.host + '/bi/finalcase/finaldata.xlsx';
+      return $timeout(function() {
+        return $window.open(exportUrl);
+      });
+    };
+    vm.searchKeypress = function ($event) {
+      if ($event.keyCode === 13) {
+        vm.pageConf.keywords = vm.query;
+        return vm.reload(true);
+      }
+    };
+    vm.canFilter = function (field) {
+      return field.filterType;
+    };
+    vm.search();
+    return this;
+  });
+}).call(this);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,104 +5,124 @@ import _ from 'lodash';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import lazypipe from 'lazypipe';
 import nodemon from 'nodemon';
+import open from 'open';
 
 const paths = {
-  client: {
-    indexView: 'client/index.html',
-    styles: 'client/{app,components}/**/*.scss',
-    mainStyle: 'client/app/app.scss',
-    scripts: 'client/**/!(*.spec|*.mock).js',
-    test: 'client/{app,components}/**/*.{spec,mock}.js'
-  },
-  server: {
-    scripts: 'server/**/!(*.spec|*.intergration).js',
-    json: 'server/**/*.json',
-    test: {
-      intergration: 'server/**/*.intergration.js',
-      unit: 'server/**/*.spec.js'
-    }
-  },
-  dist: 'dist'
+    client: {
+        indexView: 'client/index.html',
+        styles: 'client/{app,components}/**/*.scss',
+        mainStyle: 'client/app/app.scss',
+        scripts: 'client/**/!(*.spec|*.mock).js',
+        test: 'client/{app,components}/**/*.{spec,mock}.js'
+    },
+    server: {
+        scripts: 'server/**/!(*.spec|*.intergration).js',
+        json: 'server/**/*.json',
+        test: {
+            intergration: 'server/**/*.intergration.js',
+            unit: 'server/**/*.spec.js'
+        }
+    },
+    dist: 'dist'
 };
 const $ = gulpLoadPlugins();
 
 let styles = lazypipe()
-  .pipe($.sourcemaps.init)
-  .pipe($.sass)
-  .pipe($.autoprefixer, {browsers: ['last 1 version']})
-  .pipe($.sourcemaps.write, '.');
+    .pipe($.sourcemaps.init)
+    .pipe($.sass)
+    .pipe($.autoprefixer, { browsers: ['last 1 version'] })
+    .pipe($.sourcemaps.write, '.');
 
 let transpileClient = lazypipe()
-  .pipe($.sourcemaps.init)
-  .pipe($.babel, {
-    plugins: [
-      'transform-class-properties'
-    ]
-  })
-  .pipe($.sourcemaps.write, '.');
+    .pipe($.sourcemaps.init)
+    .pipe($.babel, {
+        plugins: [
+            'transform-class-properties'
+        ]
+    })
+    .pipe($.sourcemaps.write, '.');
 
 let transplieServer = lazypipe()
-  .pipe($.sourcemaps.init)
-  .pipe($.bable, {
-    plugins: [
-      'transform-class-properties',
-      'transform-runtime'
-    ]
-  })
-  .pipe($.sourcemaps.write, '.');
+    .pipe($.sourcemaps.init)
+    .pipe($.bable, {
+        plugins: [
+            'transform-class-properties',
+            'transform-runtime'
+        ]
+    })
+    .pipe($.sourcemaps.write, '.');
 
 let lintScriptClient = lazypipe()
-  .pipe($.eslint)
-  .pipe($.eslint.format)
-  .pipe($.eslint.failAfterError);
+    .pipe($.eslint)
+    .pipe($.eslint.format)
+    .pipe($.eslint.failAfterError);
 
 let lintScriptServer = lazypipe()
-  .pipe($.eslint)
-  .pipe($.eslint.format)
-  .pipe($.eslint.failAfterError);
+    .pipe($.eslint)
+    .pipe($.eslint.format)
+    .pipe($.eslint.failAfterError);
 
 // inject *.module.js sort
-function sortModulesTop (file1, file2) {
-  const module = /\.module\.js$/;
-  const fileModule1 = module.test(file1.path);
-  const fileModule2 = module.test(file2.path);
-  if (fileModule1 === fileModule2) {
-    if (file1.path < file2.path) {
-      return -1;
+function sortModulesTop(file1, file2) {
+    const module = /\.module\.js$/;
+    const fileModule1 = module.test(file1.path);
+    const fileModule2 = module.test(file2.path);
+    if (fileModule1 === fileModule2) {
+        if (file1.path < file2.path) {
+            return -1;
+        }
+        if (file1.path > file2.path) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return (fileModule1 ? -1 : 1);
     }
-    if (file1.path > file2.path) {
-      return 1;
-    }
-    else {
-      return 0;
-    }
-  } else {
-    return (fileModule1 ? -1 : 1);
-  }
+}
+
+function checkAppReady(cb) {
+    const options = {
+        host: 'localhost',
+        port: config.port
+    };
+
+    http.get(options, () => cb(true))
+        .on('error', () => cb(false));
 }
 
 // call pate until first success
-function whenServerReady (cb) => {
-  let serverReady = false;
+function whenServerReady(cb) => {
+    let serverReady = false;
+    const appReadyInterval = setInterval(() => {
+        checkAppReady((ready) => {
+            if (!ready || serverReady) {
+                return;
+            }
+            clearInterval(appReadyInterval);
+            serverReady = true;
+            cb();
+        })
+    });
 }
 
 // server log
-function onServerLog (log) {
-  console.log($.util.colors.white('[') +
-    $.util.colors.yellow('nodemon') +
-    $.util.colors.white(']') +
-    log.message);
+function onServerLog(log) {
+    console.log($.util.colors.white('[') +
+        $.util.colors.yellow('nodemon') +
+        $.util.colors.white(']') +
+        log.message);
 }
 
 gulp.task('inject', cb => $.runSequence(['inject:js', 'inject:scss'], cb));
 
 gulp.task('inject:js', () => {
-  return gulp.src(paths.client.indexView)
-    .pipe($.inject(
-      gulp.src(_.union([paths.client.scripts], ['!client/app/app.js']), {read: false})
-        .pipe($.sort(sortModulesTop))
-      ))
-    .pipe(gulp.dest('client'));
+    return gulp.src(paths.client.indexView)
+        .pipe($.inject(
+            gulp.src(_.union([paths.client.scripts], ['!client/app/app.js']), { read: false })
+            .pipe($.sort(sortModulesTop))
+        ))
+        .pipe(gulp.dest('client'));
 });
 
 // gulp.task('inject:css', () => {
@@ -119,81 +139,86 @@ gulp.task('inject:js', () => {
 // });
 
 gulp.task('inject:scss', () => {
-  return gulp.src(paths.client.mainStyle)
-    .pipe($.inject(
-      gulp.src(_.union([paths.client.styles], ['!' + paths.client.mainStyle]), {read: false})
-        .pipe($.sort()),
-        {
-          transform: (filePath) => {
-            let newPath = filePath
-              .replace('/client/app/', '')
-              .replace('/client/components/', '../components/')
-              .replace('.scss', '');
-            return `@import "${newPath}";`;
-          }
-        }))
-    .pipe(gulp.dest('client/app'));
+    return gulp.src(paths.client.mainStyle)
+        .pipe($.inject(
+            gulp.src(_.union([paths.client.styles], ['!' + paths.client.mainStyle]), { read: false })
+            .pipe($.sort()), {
+                transform: (filePath) => {
+                    let newPath = filePath
+                        .replace('/client/app/', '')
+                        .replace('/client/components/', '../components/')
+                        .replace('.scss', '');
+                    return `@import "${newPath}";`;
+                }
+            }))
+        .pipe(gulp.dest('client/app'));
 });
 
 gulp.task('styles', () => {
-  return gulp.src(paths.client.mainStyle)
-    .pipe(styles())
-    .pipe(gulp.dest('.tmp/app'));
+    return gulp.src(paths.client.mainStyle)
+        .pipe(styles())
+        .pipe(gulp.dest('.tmp/app'));
 });
 
 gulp.task('es6:client', () => {
-  return gulp.src(paths.client.scripts)
-    .pipe(transpileClient())
-    .pipe(gulp.dest('.tmp'));
+    return gulp.src(paths.client.scripts)
+        .pipe(transpileClient())
+        .pipe(gulp.dest('.tmp'));
 });
 
 gulp.task('es6:server', () => {
-  return gulp.src(_.union([paths.server.scripts], [paths.server.json]))
-    .pipe(transpileServer())
-    .pipw(gulp.dest('dist/server'));
+    return gulp.src(_.union([paths.server.scripts], [paths.server.json]))
+        .pipe(transpileServer())
+        .pipw(gulp.dest('dist/server'));
 });
 
 gulp.task('lint:scripts', cb => runSequence(['lint:scripts:client', 'lint:scripts:server'], cb));
 
 gulp.task('lint:scripts:client', () => {
-  return gulp.src(_.union([paths.client.scripts], _.map([paths.client.test], blob => '!' + blob)))
-    .pipe(lintScriptClient());
+    return gulp.src(_.union([paths.client.scripts], _.map([paths.client.test], blob => '!' + blob)))
+        .pipe(lintScriptClient());
 });
 
 gulp.task('lint:scripts:server', () => {
-  return gulp.src(_.union([paths.server.scripts], _.map([paths.server.test], blob => '!' + blob)))
-    .pipe(lintScriptServer());
+    return gulp.src(_.union([paths.server.scripts], _.map([paths.server.test], blob => '!' + blob)))
+        .pipe(lintScriptServer());
 });
 
 gulp.task('lint:script:clientTest', => {
-  return gulp.src(paths.client.test)
-    .pipe(lintScriptClient());
+    return gulp.src(paths.client.test)
+        .pipe(lintScriptClient());
 });
 
 gulp.task('lint:script:serverTest', => {
-  return gulp.src(paths.server.test)
-    .pipe(lintScriptServer());
+    return gulp.src(paths.server.test)
+        .pipe(lintScriptServer());
 });
 
-gulp.task('clean', () => del(['.tmp/**/*'], {dot: true}));
+gulp.task('clean', () => del(['.tmp/**/*'], { dot: true }));
 
 gulp.task('start:client', cb => {
-  whenServerReady(() => {
-    open('http://localhost:' + config.port);
-    cb();
-  });
+    whenServerReady(() => {
+        open('http://localhost:' + config.port);
+        cb();
+    });
 });
 
 gulp.task('start:server:dev', () => {
-  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-  config = require('./server/config/environment');
-  nodemon('--watch server server')
-    .on('log', onServerLog);
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+    config = require('./server/config/environment');
+    nodemon('--watch server server')
+        .on('log', onServerLog);
 });
 
 gulp.task('start:server:prod', () => {
-  process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-  config = require(`${paths.dist}/server/config/environment`);
-  nodemon('--watch server server')
-    .on('log', onserverLog);
+    process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+    config = require(`${paths.dist}/server/config/environment`);
+   saf nodemon('--watch server server')
+        .on('log', onserverLog);
+});
+
+gulp.task('watch', () => {
+    $.liverload.listen();
+
+    $.watch
 });
