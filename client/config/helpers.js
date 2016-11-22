73a957b8e8b35c0735d19550fe4bafc2471201b2
周群营 <richardzhou@shunshunliuxue.com>
2016-11-22 18:40:23 +0800

    Path generator.

--- client/config/helpers.js
+++ client/config/helpers.js
@@ -1,1 +1,11 @@
 import path from 'path';
+
+const _root = path.resolve(__dirname, '../..');
+
+function root() {
+  const args = Array.prototype.slice.call(arguments, 0);
+
+  return path.join.apply(path, [_root].concat(args));
+}
+
+export default root;
